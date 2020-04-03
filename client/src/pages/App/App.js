b8aa512af588678552740e452f4a8a4820a5aebd
keith <keith@raincityambience.com>
2020-04-03 00:14:35 -0400

    Continue mobile styles for News posts (home page)

--- client/src/pages/App/App.js
+++ client/src/pages/App/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     return (
       <Router>
         {/* <Routes /> */}
-        <div>
+        <div className="site-body">
           <Route exact path="/"     component={Home} />
           <Route path='/login'      component={Login} />
           <Route path='/players'    component={Players} />
