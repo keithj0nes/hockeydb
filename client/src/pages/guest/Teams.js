8cc91c92325a133f968937671743ea01e6424159
SpencerTower <spencerjtower1@gmail.com>
2022-06-19 19:25:34 -0700

    adds initial teams component

--- client/src/pages/guest/Teams.js
+++ client/src/pages/guest/Teams.js
@@ -3,5 +3,19 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getTeams } from '../../redux/slices/teams';
 
 const Teams = () => {
-    
-}
+    const dispatch = useDispatch();
+
+    const { teams, isLoading } = useSelector(state => state.teams);
+
+    useEffect(() => {
+        dispatch(getTeams());
+    }, []);
+
+    console.log('TEAMS IN COMPONENT ', teams);
+
+    return (
+        <div>
+            Display Teams Data Here
+        </div>
+    );
+};
