1c218c5faec8952bce00ce11b0407cd84c20a57d
SpencerTower <spencerjtower1@gmail.com>
2022-11-07 13:28:34 -0800

    adds initial divisions slice

--- client/src/redux/slices/divisions.js
+++ client/src/redux/slices/divisions.js
@@ -1,1 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import request from '../request';
+import { wait } from '../../utils';
+
+const initialState = {
+    divisions: [],
+    isLoading: false,
+};
+
+export const divisionsSlice = createSlice({
+    name: 'divisions',
+    initialState,
+    reducers: {
+        getInit: (state) => {
+            console.log('IS LOADING:', state.isLoading);
+            state.isLoading = true;
+        },
+        getDivisionsSuccess: (state, { payload }) => {
+            state.divisions = [...payload];
+            state.isLoading = false;
+        },
+    },
+});
+
+export const { getInit, getDivisionsSuccess } = divisionsSlice.actions;
+
+export const getDivisions =
+    (filter = '') =>
+    async (dispatch) => {
+        dispatch(getInit());
+
+        const data = await request({
+            url: `/api/divisions?${filter}`,
+            method: 'GET',
+            isPublic: true,
+        });
+
+        console.log('DIVISONS DATA', data);
+
+        // dispatch(getDivisionsSuccess(divisions));
+    };
+
+export default divisionsSlice.reducer;
